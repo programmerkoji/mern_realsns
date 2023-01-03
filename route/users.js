cd81b57c17f5927bc77800c912006c3016593fcf
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2023-01-03 20:25:19 +0900

    ユーザーの更新API

--- route/users.js
+++ route/users.js
@@ -1,7 +1,28 @@
 const router = require("express").Router();
+const User = require("../models/User");
 
-router.get("/", (req, res) => {
-	res.send("user router");
+// CRUD
+// ユーザー情報の更新
+router.put("/:id", async (req, res) => {
+	if (req.body.userId === req.params.id || req.body.isAdmin) {
+		try {
+			const user = await User.findByIdAndUpdate(req.params.id, {
+				$set: req.body,
+			});
+			res.status(200).json("ユーザー情報が更新されました");
+		} catch (err) {
+			return res.status(500).json(err);
+		}
+	} else {
+		return res.status(403).json("自分のアカウントのときだけ更新できます");
+	}
 });
+
+// ユーザー情報の削除
+// ユーザー情報の取得
+
+// router.get("/", (req, res) => {
+// 	res.send("user router");
+// });
 
 module.exports = router;
