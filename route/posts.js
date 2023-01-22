eda11b4f022e2502a0be8e89c75a0134bf26ea1c
programmerkoji <60305092+programmerkoji@users.noreply.github.com>
2023-01-22 20:52:56 +0900

    タイムラインの投稿を取得するAPI

--- route/posts.js
+++ route/posts.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Post = require("../models/Post");
+const User = require("../models/User");
 
 // 投稿を作成する
 router.post("/", async (req, res) => {
@@ -75,12 +76,27 @@ router.put("/:id/like", async (req, res) => {
 					likes: req.body.userId,
 				},
 			});
-			return res
-				.status(403)
-				.json("投稿のいいねを外しました");
+			return res.status(403).json("投稿のいいねを外しました");
 		}
 	} catch (err) {
 		res.status(500).json(err);
+	}
+});
+
+// タイムラインの投稿を取得
+router.get("/timeline/all", async (req, res) => {
+	try {
+		const currentUser = await User.findById(req.body.userId);
+		const userPosts = await Post.find({ userId: currentUser._id });
+		// 自分がフォローしている友達の投稿内容をすべて取得
+		const friendPosts = await Promise.all(
+			currentUser.followings.map((friendId) => {
+				return Post.find({ userId: friendId });
+			})
+		);
+		return res.status(200).json(userPosts.concat(...friendPosts));
+	} catch (err) {
+		return res.status(500).json(err);
 	}
 });
 
